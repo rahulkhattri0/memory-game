969244e455323d6cdf02d1da16756138c1773e1a
unknown <rahulkhattri14@gmail.com>
2022-06-04 14:10:51 +0530

    added firebase dependencies and now images can be uploaded to firestore

--- app/src/main/java/com/example/memorygame/createActivity.kt
+++ app/src/main/java/com/example/memorygame/createActivity.kt
@@ -14,6 +14,7 @@ import android.provider.MediaStore
 import android.text.Editable
 import android.text.InputFilter
 import android.text.TextWatcher
+import android.util.Log
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
@@ -27,14 +28,23 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.memorygame.models.boardsize
 import com.example.memorygame.utils.BitmapScaler
 import com.example.memorygame.utils.PICKED_BOARD_SIZE
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import java.io.ByteArrayOutputStream
 
 class createActivity : AppCompatActivity() {
+    //some constants
+    companion object{
+        const val TAG:String ="createActivity"
+    }
     private lateinit var boardsize: boardsize
     private lateinit var rvimagepicker:RecyclerView
     private lateinit var savebutton: Button
     private lateinit var textfield:EditText
     private lateinit var adapter:createactivityadapter
+    private val storage = Firebase.storage
+    private val database = Firebase.firestore
     private val gallerypicIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI).putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true).setType("image/*")
     private var  permissions : ActivityResultLauncher<Array<String>> = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()){
@@ -61,6 +71,7 @@ class createActivity : AppCompatActivity() {
         if (result.resultCode == RESULT_OK && result.data != null) {
             val data = result.data?.data
             val clippeddata = result.data?.clipData
+            //the two if conditions are null checks
             if (clippeddata != null) {
                 for (i in 0 until clippeddata.itemCount) {
                     val clipItem = clippeddata.getItemAt(i)
@@ -131,11 +142,44 @@ class createActivity : AppCompatActivity() {
     }
 
     private fun saveimagestofirebase() {
+        var failed_to_upload_image = false
+        val customgamename:String = textfield.text.toString()
+        val uploadimageurls = mutableListOf<String>()
+        //in the following for loop we have iterated using two variables and these variables iterate over the list_of_uris list
+        // and we have used the withindex() library function
         for ((index:Int , picuri:Uri) in list_of_uris.withIndex()){
                 val byteArray:ByteArray = converttoByteArray(picuri)
-
+            val filepath:String = "images/$customgamename/${System.currentTimeMillis()}-${index}.jpg"
+            //the following lines of code are the logic for uploading images to firestore
+            val photoreference = storage.reference.child(filepath)
+            //putBytes() method is used to upload the bytearray
+            //putbytes() is a longer method so continue with task as the name suggests waits for put bytes to finish and then excutes the task defined in the lambda expression
+            photoreference.putBytes(byteArray).continueWithTask { uploadtask ->
+                photoreference.downloadUrl
+            }.addOnCompleteListener{
+                downloadurltask -> if(!downloadurltask.isSuccessful){
+                    Toast.makeText(this,"failed to upload image to firestore",Toast.LENGTH_LONG)
+                failed_to_upload_image = true
+                return@addOnCompleteListener
+            }
+                // the return label used in this return statement is used for specifying which function among several nested ones this statement returns from. It works with function literals (lambdas) and local functions.
+                if(failed_to_upload_image){
+                    return@addOnCompleteListener
+                }
+            val downloadurl = downloadurltask.result.toString()
+                uploadimageurls.add(downloadurl)
+                Log.i(TAG,"image uploaded $picuri")
+            }
+            if(uploadimageurls.size == boardsize.getpairs()){
+                handleAllimagesUpload(customgamename,uploadimageurls)
+            }
         }
     }
+
+    private fun handleAllimagesUpload(text: String, uploadimageurls: MutableList<String>) {
+        //will add later
+    }
+
     //these functions to get the bitmap are provided to us by android and here
     // we are checking the android version of the device on which the app is running
     // if it is lower than android pie then something else is called and if it is android pie or greater then something else is called
@@ -147,8 +191,10 @@ class createActivity : AppCompatActivity() {
         else{
             MediaStore.Images.Media.getBitmap(contentResolver,picuri)
         }
+        Log.i(TAG,"original bitmap width ${originalbitmap.width} height ${originalbitmap.height} ")
         val scaledBitmap = BitmapScaler.scale_To_fit_height(originalbitmap,250)
-        //downscaleing the images is necessary because we only have a limited amount of free storage available in firebase
+        Log.i(TAG,"Scaled bitmap bitmap width ${scaledBitmap.width} height ${scaledBitmap.height} ")
+        //down scaling the images is necessary because we only have a limited amount of free storage available in firebase
         val byteOutputStream = ByteArrayOutputStream()
         scaledBitmap.compress(Bitmap.CompressFormat.JPEG,60,byteOutputStream)
         return byteOutputStream.toByteArray()
